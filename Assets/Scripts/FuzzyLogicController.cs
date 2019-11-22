1afc15e6c92fb46720e139149e891b492d86a3e1
Ben Strong <benjamin.strong@mymail.champlain.edu>
2019-11-22 13:38:20 -0500

    Added and textured snake model, poorly i might add. Worked on FuzzyLogicController.cs

--- Assets/Scripts/FuzzyLogicController.cs
+++ Assets/Scripts/FuzzyLogicController.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum SNAKE_STATE 
+{ 
+    SEEKING_COVER = 0,
+    NEED_HEALING,
+    ATTACKING,
+    NUM_STATES
+}
+
 public class FuzzyLogicController : MonoBehaviour
 {
-    public AnimationCurve critical, hurt, heatlthy;
+    public AnimationCurve critical, hurt, healthy;
     public float health = 100.0f;
+
+    SNAKE_STATE state;
+
+    float healthyValue = 0,
+          hurtValue = 0,
+          criticalValue = 0;
 
     void Start()
     {
@@ -15,27 +29,40 @@ public class FuzzyLogicController : MonoBehaviour
     void Update()
     {
         checkInput();
-
+        evaluateValues();
+        evaluateState();
+        commitToAction();
     }
 
     void checkInput()
     {
         if(Input.GetKeyDown(KeyCode.I))
-        {
             health -= 5f;
-        }
+        
         if(Input.GetKeyDown(KeyCode.O))
-        {
             health += 5f;
-        }
+        
         if(Input.GetKeyDown(KeyCode.P))
-        {
             health = 20f;
-        }
+        
+        if(Input.GetKeyDown(KeyCode.U))
+            health = 100f;
     }
 
     void evaluateValues() 
-    { 
-        
+    {
+        healthyValue = healthy.Evaluate(health);
+        hurtValue = hurt.Evaluate(health);
+        criticalValue = critical.Evaluate(health);
+    }
+
+    void evaluateState()
+    {
+
+    }
+
+    void commitToAction()
+    {
+
     }
 }
